60d44c0d15f88555967bd60a4553b28537649b16
HassanOkashaa <102351315+HassanOkashaa@users.noreply.github.com>
2024-11-01 17:15:18 +0200

    fix

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: process.env.DB_USER || "hassan",
   host:
     process.env.DB_HOST ||
-    "universitynodeapp-db.c9iaqc08yess.us-east-1.rds.amazonaws.com",
+    "universitynodeapp.c9iaqc08yess.us-east-1.rds.amazonaws.com",
   database: process.env.DB_NAME || "universitynodeapp",
   password: process.env.DB_PASSWORD || "postgres",
   port: process.env.DB_PORT || 5432,
